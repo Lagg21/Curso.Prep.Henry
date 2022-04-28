cf4a5dcac3775ab39699e5c48ce0b641b0ff0685
Lagg21 <leigelagg@gmail.com>
2022-04-28 02:51:40 -0300

    prueba

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -99,6 +99,7 @@ function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  return
 }
 
 function esEntero(numero) {
